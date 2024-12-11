07ef58f0dcf9dd825396422e37ac4fb052eb337e
aapopov52 <149938611+aapopov52@users.noreply.github.com>
2024-12-11 20:24:01 +0300

    Create hakaton3_server.py

---
+++ server/hakaton3_server.py
@@ -0,0 +1,169 @@
+import time
+import psycopg2
+import nltk
+from summarizer import Summarizer
+from transformers import AutoModel, AutoTokenizer, AutoConfig
+
+def run_proccess():
+    conn = psycopg2.connect(
+           host="194.87.215.7",
+           database="hakaton3",
+           user="gen_user",
+           password="KMb+9b&J(eUi~5",
+           port = '5432'
+           )
+    
+    cur = conn.cursor()
+    sql = f''' select id, usl_cod_model, dolya_usech from zayavka_main_summarise where dt_run is null and dt_out is null; '''
+    cur.execute(sql)
+    rows = cur.fetchall()
+    # записей нет - выходим
+    if len(rows) == 0:
+        cur.close()
+        conn.close()
+        return
+    
+    id_zayavka_main_summarise = rows[0][0]
+    usl_cod_model = rows[0][1]
+    dolya_usech = rows[0][2]
+    
+    text_otl = []
+    text_neitral = []
+    text_bad = []
+    
+    text_otl_in = ''
+    text_neitral_in = ''
+    text_bad_in = ''
+    
+    text_otl_out = ''
+    text_neitral_out = ''
+    text_bad_out = ''
+    
+    i_cnt5 = 0
+    i_cnt4 = 0
+    i_cnt3 = 0
+    i_cnt2 = 0
+    i_cnt1 = 0
+    i_notball = 0
+    
+    for row in rows:
+        sql = f''' select rating, text
+                    from zayavka_main_summarise_detal t1
+                    left join otziv t2 on t2.id = t1.id_otziv
+                    where t1.id_zayavka_main_summarise = {id_zayavka_main_summarise}; '''
+        cur.execute(sql)
+        rows = cur.fetchall()
+        if len(rows) > 0:
+            for row in rows:
+                if row[0] == 5:
+                    text_otl.append(row[1])
+                    i_cnt5 += 1
+                elif row[0] == 4:    
+                    text_neitral.append(row[1])
+                    i_cnt4 += 1
+                elif row[0] == 3:    
+                    text_bad.append(row[1])
+                    i_cnt3 += 1
+                elif row[0] == 2:    
+                    text_bad.append(row[1])
+                    i_cnt2 += 1
+                elif row[0] == 1:    
+                    text_bad.append(row[1])
+                    i_cnt1 += 1
+                else:    
+                    text_neitral.append(row[1])
+                    i_notball += 1
+    
+    if len(text_otl) > 0:
+        for s1 in text_otl:
+            if text_otl_in != '': text_otl_in += '\n'
+            text_otl_in = text_otl_in + str(s1)
+    else:
+        text_otl_in = 'отсутствуют'
+    
+    if len(text_neitral) > 0:
+        for s1 in text_neitral:
+            if text_neitral_in != '': text_neitral_in += '\n'
+            text_neitral_in = text_neitral_in + str(s1)
+    else:
+        text_neitral_in = 'отсутствуют'
+    
+    if len(text_bad) > 0:
+        for s1 in text_bad:
+            if text_bad_in != '': text_bad_in += '\n'
+            text_bad_in = text_bad_in + str(s1)
+    else:
+        text_bad_in = 'отсутствуют'
+    
+    if text_otl_in != 'отсутствуют':
+        text_otl_out = get_summ_text(usl_cod_model, dolya_usech, text_otl_in)
+    else:
+        text_otl_out = text_otl_in
+    if text_neitral_in != 'отсутствуют':
+        text_neitral_out = get_summ_text(usl_cod_model, dolya_usech, text_neitral_in)
+    else:
+        text_neitral_out = text_neitral_in
+    if text_bad_in != 'отсутствуют':
+        text_bad_out = get_summ_text(usl_cod_model, dolya_usech, text_bad_in)
+    else:
+        text_bad_out = text_bad_in
+        
+    s_out = f'''Статистика по отзывам: 
+   - положительные (5): {i_cnt5} 
+   - нейтральные (4, не указан): {i_cnt4 + i_notball} (4 - {i_cnt4}, не указан - {i_notball}) 
+   - отрицательные (1, 2, 3): {i_cnt1 + i_cnt2 + i_cnt3} (3 - {i_cnt3}, 2 - {i_cnt2}, 1 - {i_cnt1}) 
+
+Положительные:
+{text_otl_out}
+
+Нейтральные:
+{text_neitral_out}
+
+Отрицательные:
+{text_bad_out}'''
+    
+    s_out = s_out.replace("'", "''")
+    
+    sql = f'''insert into zayavka_main_summarise_result (id_zayavka_main_summarise,result)
+                values ({id_zayavka_main_summarise}, '{s_out}');
+            update zayavka_main_summarise set dt_out = now() where id = {id_zayavka_main_summarise}; '''
+    
+    cur.execute(sql)
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+
+def get_summ_text(usl_cod_model, dolya_usech, text_in):
+    # usl_cod_model = 'gpt', 'bert', 'rubert' # имя модели
+    # dolya_usech = 90 # на сколько ужать (какую долю отсечь)
+    nltk.download('punkt')
+    nltk.download('punkt_tab')
+    n = int(len(nltk.sent_tokenize(text_in)))
+    size1 = int(round(n*(float(dolya_usech)/100)) + 1)
+    size = int((n - size1) + 1)
+    if usl_cod_model == 'gpt':
+        name_model = 'sberbank-ai/rugpt3small_based_on_gpt2'
+    if usl_cod_model == 'bert':
+        name_model = 'bert-base-multilingual-cased'
+    if usl_cod_model == 'rubert':
+        name_model = 'DeepPavlov/rubert-base-cased'
+    
+    config = AutoConfig.from_pretrained(name_model, output_hidden_states = True)
+    custom_tokenizer = AutoTokenizer.from_pretrained(name_model)
+    custom_model = AutoModel.from_pretrained(name_model, config=config)
+    model = Summarizer(custom_model=custom_model, custom_tokenizer=custom_tokenizer)
+    
+    result = model(body=text_in, num_sentences=size)
+    text_out  = ''.join(result)
+
+    return text_out
+
+
+def Main():    
+    while 1 == 1:
+        run_proccess()
+        time.sleep(1)
+
+
+Main()
