4b4b4ddba3d21d3e19fa3c430fbf6cfa85a84c81
aapopov52 <149938611+aapopov52@users.noreply.github.com>
2024-12-14 20:15:35 +0300

    Add files via upload
    дополнение для тестирования суммаризации

--- database/таблицы.sql
+++ database/таблицы.sql
@@ -61,8 +61,12 @@ create table zayavka_main_summarise (
 		dt_load TIMESTAMP,
 		dt_run TIMESTAMP,
 		dt_out TIMESTAMP,
+		b_test bool,
+		id_object_main int,
 		CONSTRAINT zayavka_main_summarise_pkey PRIMARY KEY (id)
-		);	
+		);	 
+
+	
 --drop table zayavka_main_summarise_detal;
 create table zayavka_main_summarise_detal (
 		id serial not null,
@@ -83,5 +87,46 @@ create table zayavka_main_summarise_result (
 ALTER TABLE public.zayavka_main_summarise_result ADD CONSTRAINT zayavka_main_summarise_result__id_zayavka_main_summarise FOREIGN KEY (id_zayavka_main_summarise) REFERENCES public.zayavka_main_summarise(id) ON DELETE CASCADE;
 CREATE INDEX idx_zayavka_main_summarise_result_id_zayavka_main_summarise ON zayavka_main_summarise_result (id_zayavka_main_summarise);
 
+-- таблица тестирования
+drop table zayavka_main_summarise_test 
+create table zayavka_main_summarise_test (
+		id serial not null,
+		id_object_main int,
+		usl_cod_model varchar(10), 
+		dolya_usech int,
+		text_otl_in text,
+    	text_neitral_in text,
+    	text_bad_in text,
+	    text_otl_out text,
+    	text_neitral_out text,
+    	text_bad_out text,
+    	metrika1 numeric(5, 2),
+    	metrika2 numeric(5, 2),
+    	metrika3 numeric(5, 2),
+		CONSTRAINT zayavka_main_summarise_test_pkey PRIMARY KEY (id)
+		);
+	/*
+select count(*) from zayavka_main_summarise order by dt_run asc;
+
+delete from zayavka_main_summarise where id_object_main = 122960
+select * from zayavka_main_summarise_test order by id_object_main;
+delete from zayavka_main_summarise_test where id = 10
+
+select * from  zayavka_main_summarise 
+
+-- информация для теста
 
 select * from zayavka_main_summarise
+insert into zayavka_main_summarise (usl_cod_model,dolya_usech,dt_load,id_object_main)
+				select 'gpt', 50, now(), tm.id 
+				from object_main tm
+				left join zayavka_main_summarise_test t1 on t1.id_object_main = tm.id
+				left join zayavka_main_summarise t2 on t2.id_object_main = tm.id
+				where cnt_otziv >= 10 and t1.id is null and t2.id is null
+				
+				
+
+select count(*) from object_main where cnt_otziv >= 10
+
+*/
+
